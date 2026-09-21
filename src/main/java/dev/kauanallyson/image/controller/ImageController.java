3cf48c5473e4ef43785582c58de641115bb83f6a
Kauan <kauanallysonrp@gmail.com>
2026-09-21 15:23:25 -0300

    feat: update controller to fit new storage port

--- src/main/java/dev/kauanallyson/image/controller/ImageController.java
+++ src/main/java/dev/kauanallyson/image/controller/ImageController.java
@@ -1,23 +1,24 @@
 package dev.kauanallyson.image.controller;
 
-import dev.kauanallyson.image.model.Image;
+import dev.kauanallyson.image.dto.ImageResponse;
+import dev.kauanallyson.image.dto.ImageUploadResponse;
 import dev.kauanallyson.image.service.ImageService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/api/v1/images")
-public final class ImageController {
+public class ImageController {
     private final ImageService imageService;
 
     public ImageController(ImageService imageService) {
@@ -25,23 +26,31 @@ public final class ImageController {
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Image> uploadImage(@RequestPart("image") MultipartFile file) {
-        Image image = imageService.uploadImage(file);
-        return ResponseEntity.created(image.getUri()).build();
+    public ResponseEntity<ImageUploadResponse> uploadImage(
+            @RequestHeader("X-File-SHA256") String hash,
+            @RequestPart("file") MultipartFile file
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(imageService.uploadImage(hash, file));
     }
 
     @GetMapping
-    public ResponseEntity<PagedModel<Image>> getAllImages(
-            @PageableDefault(size = 20, sort = "uuid", direction = Sort.Direction.DESC)
+    public ResponseEntity<PagedModel<ImageResponse>> getAllImages(
+            @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC)
             Pageable pageable
     ) {
-        Page<Image> page = imageService.getAllImages(pageable);
+        Page<ImageResponse> page = imageService.getAllImages(pageable);
         return ResponseEntity.ok(new PagedModel<>(page));
     }
 
-    @GetMapping("/{uuid}")
-    public ResponseEntity<Image> findImageByUUID(@PathVariable String uuid){
-        Image image = imageService.findImageByUUID(UUID.fromString(uuid));
-        return ResponseEntity.ok(image);
+    @GetMapping("/{hash}")
+    public ResponseEntity<ImageUploadResponse> findImageByHash(@PathVariable String hash) {
+        return ResponseEntity.ok(imageService.findImageByHash(hash));
+    }
+
+    @DeleteMapping("/{hash}")
+    public ResponseEntity<HttpStatus> deleteImageByHash(@PathVariable String hash) {
+        imageService.deleteImageByHash(hash);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
